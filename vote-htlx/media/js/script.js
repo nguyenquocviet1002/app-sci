33b685509eb1d858444f683e4fcb7d5a7b4d03b8
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-03-30 16:58:07 +0700

    update

--- vote-htlx/media/js/script.js
+++ vote-htlx/media/js/script.js
@@ -1,5 +1,5 @@
 const baseURL = 'https://app.scigroupvn.com/vote/public/api';
-const ipLocal = '222.252.21.4';
+const ipLocal = '222.252.21.7';
 
 const idUser = document.getElementsByClassName('vote__button')[0].getAttribute('data-id');
 const dataUser = [];
@@ -9,8 +9,6 @@ const submitVote = async () => {
     const phoneElm = document.getElementById('phone');
     const errType = document.getElementsByClassName('err__type');
     const phoneN = document.getElementById('phone').value;
-    const ipLocal = document.getElementById('ipLocal').value;
-    const idUserKN = document.getElementById('idUser').value;
 
     const phone_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
     if (phoneN !== '') {
@@ -24,18 +22,25 @@ const submitVote = async () => {
                     .then(data => (data))
             }
             const dataNew = await dataVotes();
-            const checkUser = dataNew.filter(item => (item.userid === dataUser[0].data.userid));
+            const checkUser = dataNew.filter(item => (item.userid === idUser));
             const checkPhone = checkUser.filter(item => (item.phone === phoneN));
-            if (checkPhone.length > 0) {
-                phoneElm.classList.add('err');
-                errType[0].innerHTML = 'Số điện thoại đã thực hiện lượt vote'
+            const checkIp = checkUser.filter(item => (item.ip === ipLocal));
+            if (checkPhone.length > 0 || checkIp.length > 0) {
+                document.getElementsByClassName('vote__button')[0].classList.add('disabled');
+                document.getElementsByClassName('vote__button')[0].innerHTML = 'Đã bình chọn';
+                localStorage.setItem('tokenUser', idUser);
+                closeForm();
+                document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend', formWarning());
+                setTimeout(() => {
+                    document.getElementById('modal-warning').remove()
+                }, 3000)
             }
             else {
                 phoneElm.classList.remove('err');
                 const dataVote = {
                     phone: phoneN,
                     ip: ipLocal,
-                    userid: idUserKN
+                    userid: idUser
                 }
                 await fetch(`${baseURL}/create-uservotes`, {
                     method: "POST",
@@ -45,7 +50,16 @@ const submitVote = async () => {
                     body: JSON.stringify(dataVote)
                 })
                     .then(res => res.json())
-                    .then(() => (document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend', formSuccess())))
+                    .then(() => {
+                        document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend', formSuccess());
+                        document.getElementsByClassName('vote__button')[0].classList.add('disabled');
+                        document.getElementsByClassName('vote__button')[0].innerHTML = 'Đã bình chọn';
+                        localStorage.setItem('tokenUser', idUser)
+                        setTimeout(() => {
+                            document.getElementById('modal-pop').remove()
+                            document.getElementById('modal-success').remove()
+                        }, 3000)
+                })
                 countVote += 1;
                 document.getElementsByClassName('vote__count')[0].innerHTML = countVote;
                 const bodyVote = {
@@ -69,10 +83,6 @@ const submitVote = async () => {
 const closeForm = () => {
     document.getElementById('modal-pop').remove()
 }
-const closeSuccess = () => {
-    document.getElementById('modal-pop').remove()
-    document.getElementById('modal-success').remove()
-}
 
 const formInput = () => {
     return `
@@ -88,7 +98,7 @@ const formInput = () => {
                         <input type="text" id="phone" />
                         <span class="err__type"></span>
                         <input type="hidden" value="${ipLocal}" id="ipLocal" />
-                        <input type="hidden" value="${dataUser[0].data.userid}" id="idUser" />
+                        <input type="hidden" value="${idUser}" id="idUser" />
                     </div>
                     <button type="button" class="btn__submitVote" onclick={submitVote()}>Bình chọn</button>
                 </div>
@@ -100,14 +110,28 @@ const formInput = () => {
 const formSuccess = () => {
     return `
         <div class="modal modal__vote" id="modal-success" style="display: flex">
-            <div class="modal-bg" onclick="closeSuccess()"></div>
+            <div class="modal-bg"></div>
             <div class="modal-box animate-pop">
                 <div class="modal-header modal__voteHead">
-                    <div class="modal-close" onclick="closeSuccess()">&times;</div>
                 </div>
                 <div class="modal-body">
-                    <img width="320" height="320" src="media/images/icon-success.png" alt=""/>
+                    <img width="105" height="105" src="media/images/icon-success.png" alt=""/>
                     <p class="notify">Vote thành công</p>
+                </div>
+            </div>
+        </div>
+    `
+};
+
+const formWarning = () => {
+    return `
+        <div class="modal modal__vote" id="modal-warning" style="display: flex">
+            <div class="modal-bg"></div>
+            <div class="modal-box animate-pop">
+                <div class="modal-header modal__voteHead"></div>
+                <div class="modal-body">
+                    <img width="105" height="105" src="media/images/icon-warning.png" alt=""/>
+                    <p class="notify">Bạn đã thực hiện vote không thể vote tiếp</p>
                 </div>
             </div>
         </div>
@@ -128,7 +152,12 @@ window.onload = async () => {
         .then(res => res.json())
         .then(data => (dataUser.push(data)))
     document.getElementsByClassName('vote__button')[0].classList.remove('disabled');
-    countVote = Number(dataUser[0].data.vote);
+    countVote = Number(dataUser[0].vote);
     document.getElementsByClassName('vote__count')[0].innerHTML = countVote;
+    const checkVote = localStorage.getItem('tokenUser');
+    if(checkVote === dataUser[0].userid){
+        document.getElementsByClassName('vote__button')[0].classList.add('disabled');
+        document.getElementsByClassName('vote__button')[0].innerHTML = 'Đã bình chọn';
+    }
 }
 
